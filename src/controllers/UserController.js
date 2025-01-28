64c86831b0f3bc38c14ab5699ea7d52ffd613dcf
Elisa Drumond <elisadrmnd@gmail.com>
2025-01-28 18:57:26 -0300

    refact: refactoring userController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,9 +18,9 @@ module.exports = {
     getUserById(request, response) {
         const { id } = request.params;
 
-        const user = users.find((user) => user.id === Number(id));
+        const userExists = users.find((user) => user.id === id);
 
-        if (!user) {
+        if (!userExists) {
             return response.send(400, { error: 'User not found' });
         }
 
@@ -30,6 +30,7 @@ module.exports = {
     createUser(request, response) {
         const { body } = request
         const lastUserId = users[users.length - 1].id;
+
         const newUser = {
             id: lastUserId + 1,
             name: body.name,
