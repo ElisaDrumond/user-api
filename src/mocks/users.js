da4c01ff7fdfd414a6c01417f0a40a115d60ddf4
Elisa Drumond <elisadrmnd@gmail.com>
2025-01-28 17:13:23 -0300

    Initial commit

---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Elisa'
+    },
+    {
+        id: 2,
+        name: 'Patrick'
+    },
+    {
+        id: 3,
+        name: 'Hadja'
+    },
+    {
+        id: 4,
+        name: 'Hugo'
+    },
+    {
+        id: 5,
+        name: 'Thomas'
+    },
+]
